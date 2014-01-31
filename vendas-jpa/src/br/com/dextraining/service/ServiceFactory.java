4a7f61b843f9f0b4ed9c649f27fd6bbefec38048
wapmesquita <wapmesquita@github.com>
2014-01-31 09:30:29 -0200

    preparando problema

--- vendas-jpa/src/br/com/dextraining/service/ServiceFactory.java
+++ vendas-jpa/src/br/com/dextraining/service/ServiceFactory.java
@@ -1,13 +1,18 @@
 package br.com.dextraining.service;
 
-import java.lang.reflect.Proxy;
 
 public class ServiceFactory {
 
 	@SuppressWarnings("unchecked")
 	public static <T> T service(Class<T> clazz) {
-		return (T) Proxy.newProxyInstance(ServiceFactory.class.getClassLoader(),
-				new Class<?>[] { clazz }, new ServiceHook());
+		String name = clazz.getName();
+		String pack = name.substring(0, name.indexOf(clazz.getSimpleName()));
+
+		try {
+			return (T) Class.forName(pack + "impl." + clazz.getSimpleName() + "Impl").newInstance();
+		} catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
+			throw new RuntimeException(e);
+		}
 	}
 
 }
